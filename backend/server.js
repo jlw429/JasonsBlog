2da7db268e87853d07dff3da686b7e9cf7ea95b5
jlw429 <jlw00329@gmail.com>
2021-05-02 22:35:59 -0400

    added env

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: 'jason123',
-  database: 'jasons_blog',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
